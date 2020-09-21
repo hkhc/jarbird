1e95b91b8863343bc985e2069e73bc3b9ee4d5ae
Herman Cheung <herman.kh.cheung@gmail.com>
2020-09-21 10:57:25 +0800

    rename plugin module to gradlePlugin

--- gradlePlugin/b2.build.gradle.kts
+++ gradlePlugin/b2.build.gradle.kts
@@ -50,8 +50,8 @@ version = "1.0.0"
  It is needed to make sure every version of java compiler to generate same kind of bytecode.
  Without it and build bootstrap with java 8+ compiler, then the Jarbird build with java 8
  will get error like this:
-   > Unable to find a matching variant of io.hkhc.gradle:jarbirdbootstrap:1.0.0:
-      - Variant 'apiElements' capability io.hkhc.gradle:jarbirdbootstrap:1.0.0:
+   > Unable to find a matching variant of io.hkhc.gradle:jarbird-bootstrap:1.0.0:
+      - Variant 'apiElements' capability io.hkhc.gradle:jarbird-bootstrap:1.0.0:
           - Incompatible attributes:
               - Required org.gradle.jvm.version '8' and found incompatible value '13'.
               - Required org.gradle.usage 'java-runtime' and found incompatible value 'java-api'.
